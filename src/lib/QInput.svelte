3349225d3f830770ff512ffab2f0af5b01e2e35a
Lorix <ciminata08@gmail.com>
2023-07-05 00:00:11 +0200

    PasswordVisible and Icon implementation

--- src/lib/QInput.svelte
+++ src/lib/QInput.svelte
@@ -1,11 +1,21 @@
-<script>
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+    import "iconify-icon";
+
     export let label;
     export let value = '';
     export let placeholder = '';
+    export let type: 'text' | 'password' = 'text'
     export let error = false;
+
+    let passwordVisible = false;
+    const toggleType = () => {
+        passwordVisible = !passwordVisible;
+        return type === 'text' ? type = 'password' : type = 'text';
+    };
 </script>
 
-<div class="w-full flex flex-col mb-2 last:mb-0">
+<div class="w-full flex flex-col mb-2 last:mb-0 relative">
     {#if label}
     <label
         class="ml-1 font-semibold text-base transition-colors duration-300"
@@ -15,12 +25,23 @@
         { label }
     </label>
     {/if}
-
-    <input
-        class="w-full bg-dark border-[3px] outline-none rounded-lg shadow-lg px-3 py-2 font-medium text-white transition-colors duration-300"
-        class:border-yellow={!error}
-        class:border-red={error}
-
-        type="text" bind:value {placeholder}
-    />
+ 
+    <div class="relative">
+        <input
+            class="w-full h-12 bg-dark border-[3px] outline-none rounded-lg shadow-lg px-3 py-2 font-medium text-white transition-colors duration-300"
+            class:border-yellow={!error}
+            class:border-red={error}
+            
+            autocomplete="off"
+            {...{type}} {placeholder}
+        
+            bind:value
+        />
+         
+        {#if (type === 'password' && value.length > 0) || passwordVisible}
+        <button on:click={toggleType} class="absolute top-1 right-4 bottom-0 cursor-pointer hover:opacity-60 text-yellow text-lg">
+            <iconify-icon height="none" icon={type === 'password' ? 'mdi:eye-off' : 'mdi:eye'} class="h-4" />
+        </button>
+        {/if}
+    </div>
 </div>
