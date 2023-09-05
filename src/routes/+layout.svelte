37cc281e4659dacb7c20124244cd8a555f8ff928
Lorix <ciminata08@gmail.com>
2023-09-06 01:04:52 +0200

    navbar fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
     import '../app.css';
     import QNavbar from '$lib/QNavbar.svelte';
-
+    import { page } from '$app/stores';
 </script>
 
 <div class="relative w-full h-screen flex items-center justify-center bg-dark">
-    <QNavbar />
+    {#if !$page.url.pathname.includes('/login') && !$page.url.pathname.includes('/register')}
+        <QNavbar />
+    {/if}
     <slot />
 </div>
